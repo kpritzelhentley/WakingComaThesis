12c2ec0bc9edb7366c0050accbeb02e54a8d1184
Kyra Pritzel-Hentley <kpritzelhentley@tech-fak.uni-bielefeld.de>
2018-05-30 11:03:12 +0200

    change to mean over all p300 segemnts and not over all channels

--- python_scripts/P300_average_plots.py
+++ python_scripts/P300_average_plots.py
@@ -3,38 +3,36 @@ from numpy import matlib
 import matplotlib
 import matplotlib.pyplot as plt
 
+srate = 2048
 # EEG data with srate of 1/2048
-data = np.load('/vol/nct/data/Elim_BMBF/npy/VP07_17.07_VM.bdf.mat_data.npy')
+data = np.load('/vol/nct/data/Elim_BMBF/npy/VP1_10.07_NM.bdf.mat_data.npy')
 # Array with beginning of events timestamps
-trigger = np.load('/vol/nct/data/Elim_BMBF/npy/VP07_17.07_VM.bdf.mat_trigger.npy')
+trigger = np.load('/vol/nct/data/Elim_BMBF/npy/VP1_10.07_NM.bdf.mat_trigger.npy')
 trigger = trigger.ravel()
 # labels for each trigger:
 # 1:Haeufiger Ton, 2:seltener Ton, 3:Novels, 4:Satzanfaenge, 5:Sinnvolle Satzenden, 6:Sinnlose Satzenden
-labels = np.load('/vol/nct/data/Elim_BMBF/npy/VP07_17.07_VM.bdf.mat_label.npy')
+labels = np.load('/vol/nct/data/Elim_BMBF/npy/VP1_10.07_NM.bdf.mat_label.npy')
 labels = labels.ravel()
 
 ## extract data segments for novel triggers
 # using 'flatnonzero' instead of 'where' to have 1d array (not a one-element-tuple) as output
 novels_idx = np.flatnonzero(labels == 3)
-# this list will be filled with arrays which represent the wanted data segments
-p300_sgmts = []
-
-# calculate average of all data channels
-data_sum = np.empty(data.shape[1])
-for i in range(data.shape[0]):
-    data_sum += data[i]
-# divide the sum by number of channels
-data_avg = data_sum / data.shape[0]
+# array with averaged p300 segements for each channel
+p300_sgmts = np.empty_like([data.shape[0], novels_idx.size])
 
 print('collecting data segments for novels...')
-for i in range(novels_idx.size):
-    # labels and trigger have the same size, therefore the novels_idxs can be used
-    # as trigger idxs
-    trig_idx = novels_idx[i]
-    # take the first row of data (channel 1) and get the trigger timestamps
-    # at the novels_idxs. One segment should be about one second long (from one
-    # trigger to the next)
-    p300_sgmts.append(data_avg[trigger[trig_idx]:trigger[trig_idx+1]])
+for k in range (data.shape[0]):
+    for i in range(novels_idx.size):
+        # labels and trigger have the same size, therefore the novels_idxs can be used
+        # as trigger idxs
+        trig_idx = novels_idx[k]
+        # take the first row of data (channel 1) and get the trigger timestamps
+        # at the novels_idxs. One segment should be about one second long (from one
+        # trigger to the next)
+        p300_sgmts[k][i] = data[k][trigger[trig_idx]:trigger[trig_idx]+srate]
+    # calculate mean of all segments in one channel (mean of numbers inside a row)
+    p300_mean = np.mean(p300_sgmts, axis=1)
+
 
 # plot all segments in subplots
 print('plotting and saving data segments...')
