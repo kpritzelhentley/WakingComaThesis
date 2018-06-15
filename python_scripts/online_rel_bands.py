6a2b6b41e12adc81901b20bfcc245f2a295f919f
Kyra Pritzel-Hentley <kpritzelhentley@tech-fak.uni-bielefeld.de>
2018-06-15 16:15:10 +0200

    Streaming implementation

--- python_scripts/online_rel_bands.py
+++ python_scripts/online_rel_bands.py
@@ -1,37 +1,25 @@
-from __future__ import division
+import pdb
 import numpy as np
 import time
 from pylsl import StreamInfo, StreamOutlet
-from pylsl import StreamInlet, resolve_stream
-
 
 print "loading data..."
 data = np.load('/vol/nct/data/Elim_BMBF/npy/VP07_17.07_VM.bdf.mat_data.npy')
 
-# give information about the stream
+# first create a new stream info (here we set the name to BioSemi,
+# the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
+# last value would be the serial number of the device or some other more or
+# less locally unique identifier for the stream as far as available (you
+# could also omit it but interrupted connections wouldn't auto-recover)
+
 # (name of stream, content type, channels, srate, channel format, source id)
-info = StreamInfo('EEG', 'EEG', 32, 2048, 'float32', '')
+info = StreamInfo('DoC', 'EEG', 32, 2048, 'float32', 'myuid34234')
 
 # create outlet
 outlet = StreamOutlet(info)
 
 print 'sending data...'
 # push out every sample individually
-for i in range(10):
-    outlet.push_sample(data[:,i])
-    time.sleep(0.01)
-
-# first resolve an EEG stream on the lab network
-print("looking for an EEG stream...")
-streams = resolve_stream(info)
-
-# create a new inlet to read from the stream
-print("creating inlet...")
-inlet = StreamInlet(streams[0])
-
-print("pushing samples...")
-while True:
-    # get a new sample (you can also omit the timestamp part if you're not
-    # interested in it)
-    sample = inlet.pull_sample()
-    print(sample)
+for i in range(data.shape[1]):
+    outlet.push_sample(data[:,i].tolist())
+    time.sleep(2)
--- python_scripts/plot_channel_stream.py
+++ python_scripts/plot_channel_stream.py
@@ -1,25 +1,38 @@
 from pylsl import StreamInlet, resolve_stream
-import ubicpy.sigproc as sigproc
-import numpy as np
-
-winsize = 1025 # 5 sec
-overlap = 512 # 2.5 sec
+import matplotlib.pyplot as plt
+import matplotlib.animation as anmt
+import time
 
 # first resolve an EEG stream on the lab network
 print("looking for an EEG stream...")
 streams = resolve_stream('type', 'EEG')
+
 # create a new inlet to read from the stream
 inlet = StreamInlet(streams[0])
+counter = 0
 
-# data = np.zeros(32, winsize + overlap * 2)
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+plt.xlabel('Time')
+plt.ylabel('Micro Volts')
+plt.title('EEG Channel 1')
+
 data = []
+time = []
 
 print("pull samples...")
 while True:
     # get a new sample (you can also omit the timestamp part if you're not
     # interested in it)
     sample = inlet.pull_sample()
+    counter += 1
 
-    # build a list with all incoming channel columns
-    data.append(sample[0])
-    print data
+    data.append(sample[0][0])
+    time.append(counter)
+
+    ax1.clear()
+    ax1.plot(time,data)
+    plt.pause(0.05)
+
+    print(sample)
+plt.show()
